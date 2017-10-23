82e0d61d92bce34e1b577a60145e6934428936de
vsBradleyDekker <brad@vealesolutions.com.au>
2017-10-23 17:03:52 +1100

    menu drop down
    Created menu drop down added styles and added a test page for new ideas

--- public/page-components/searchbar.php
+++ public/page-components/searchbar.php
@@ -1,10 +1,295 @@
-<section class="searchbar pt-large pb-large">
-    <div class="row align-middle">
-        <div class="columns small-12 medium-8">
+<section class="searchbar">
+    <div class="row align-middle searchbar__container">
+        <div class="columns small-12 medium-10 searchbar__search">
             <input placeholder="Search...">
         </div>
-        <div class="columns small-12 medium-4">
-            <span> A to Z index <i class="fa fa-sort-desc" aria-hidden="true"></i></span>
+        <div class="columns small-12 medium-2 searchbar__dropdown showhim  pt-large pb-large">
+            <span>A to Z index<i class="fa fa-sort-desc" aria-hidden="true"></i></span>
         </div>
+        <div class="columns small-12 medium-12 mega-menu showme">
+            <div class="row">
+                <div class="columns small-12 medium-2">
+                    <h4>A</h4>
+                    <ul>
+                        <li>
+                            <a href="#">Aquatica Marine Hooding</a>
+                        </li>
+                        <li>
+                            <a href="#">Astra</a>
+                        </li>
+                        <li>
+                            <a href="#">Astra Outdoor</a>
+                        </li>
+                        <li>
+                            <a href="#">Atlantic Blockout</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="columns small-12 medium-2">
+                    <h4>D</h4>
+                    <ul>
+                        <li>
+                            <a href="#">Dickson ALTO</a>
+                        </li>
+                        <li>
+                            <a href="#">Dickson MAX</a>
+                        </li>
+                        <li>
+                            <a href="#">Dickson Orchestra</a>
+                        </li>
+                        <li>
+                            <a href="#">DX12 Canvas</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="columns small-12 medium-2">
+                    <h4>M</h4>
+                    <ul>
+                        <li>
+                            <a href="#">Mansel</a>
+                        </li>
+                        <li>
+                            <a href="#">Marine Clear PVC</a>
+                        </li>
+                        <li>
+                            <a href="#">Maritime Vinyl - Furniture</a>
+                        </li>
+                        <li>
+                            <a href="#">Maritime Vinyl - Marine</a>
+                        </li>
+                        <li>
+                            <a href="#">Moleskin</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="columns small-12 medium-2">
+                    <h4>S</h4>
+                    <ul>
+                        <li>
+                            <a href="#">Seychelles Plus Internal</a>
+                        </li>
+                        <li>
+                            <a href="#">Simbel Internal</a>
+                        </li>
+                        <li>
+                            <a href="#">St Clair Arcade</a>
+                        </li>
+                        <li>
+                            <a href="#">St Clair Blocklight</a>
+                        </li>
+                        <li>
+                            <a href="#">St Clair LAC650</a>
+                        </li>
+                        <li>
+                            <a href="#">St Clair Sunblock</a>
+                        </li>
+                        <li>
+                            <a href="#">Sunbrella</a>
+                        </li>
+                        <li>
+                            <a href="#">Sunbrella Drapery</a>
+                        </li>
+                        <li>
+                            <a href="#">Sunbrella Upholstery</a>
+                        </li>
+                        <li>
+                            <a href="#">Sunbrella ICON</a>
+                        </li>
+                        <li>
+                            <a href="#">Sunbrella Marine</a>
+                        </li>
+                        <li>
+                            <a href="#">Sunbrella Marine Upholstery</a>
+                        </li>
+                        <li>
+                            <a href="#">Sunbrella Shift</a>
+                        </li>
+                        <li>
+                            <a href="#">Sunworker</a>
+                        </li>
+                        <li>
+                            <a href="#">Sunworker Cristal</a>
+                        </li>
+                        <li>
+                            <a href="#">Sunworker Open</a>
+                        </li>
+                        <li>
+                            <a href="#">Superscreen Flyscreen Mesh...</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="columns small-12 medium-2">
+                    <h4>V</h4>
+                    <ul>
+                        <li>
+                            <a href="#">Vistaweave</a>
+                        </li>
+                        <li>
+                            <a href="#">Vue Sunscreen mesh</a>
+                        </li>
+                    </ul>
+                </div>
+            
+                <div class="columns small-12 medium-2">
+                    <h4>B</h4>
+                    <ul>
+                        <li>
+                            <a href="#">Baltic Plus Internal</a>
+                        </li>
+                        <li>
+                            <a href="#">Billabong Canvas</a>
+                        </li>
+                        <li>
+                            <a href="#">Breezeway Mesh</a>
+                        </li>
+                        <li>
+                            <a href="#">Brella Classic Canvas</a>
+                        </li>
+                        <li>
+                            <a href="#">Bullduck TT Canvas</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="columns small-12 medium-2">
+                    <h4>G</h4>
+                    <ul>
+                        <li>
+                            <a href="#">GB550 PVC Collection</a>
+                        </li>
+                        <li>
+                            <a href="#">GB600 PVC Collection</a>
+                        </li>
+                        <li>
+                            <a href="#">GB650 PVC Collection</a>
+                        </li>
+                        <li>
+                            <a href="#">GB900 PVC Collection</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="columns small-12 medium-2">
+                    <h4>O</h4>
+                    <ul>
+                        <li>
+                            <a href="#">Oilskin</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="columns small-12 medium-2">
+                    <h4>Z</h4>
+                    <ul>
+                        <li>
+                            <a href="#">Zen Internal Fabric</a>
+                        </li>
+                    </ul>
+                </div>
+            
+                <div class="columns small-12 medium-2">
+                    <h4>C</h4>
+                    <ul>
+                        <li>
+                            <a href="#">Calico</a>
+                        </li>
+                        <li>
+                            <a href="#">Camo Canvas</a>
+                        </li>
+                        <li>
+                            <a href="#">Cannonball swag Canvas</a>
+                        </li>
+                        <li>
+                            <a href="#">Canvacon 5000E and 7000E</a>
+                        </li>
+                        <li>
+                            <a href="#">Carnaby Plus Internal</a>
+                        </li>
+                        <li>
+                            <a href="#">Clear PVC</a>
+                        </li>
+                        <li>
+                            <a href="#">Commercial 95</a>
+                        </li>
+                        <li>
+                            <a href="#">Companion Camping Can...</a>
+                        </li>
+                        <li>
+                            <a href="#">Coolabah Canvas</a>
+                        </li>
+                        <li>
+                            <a href="#">CS12 Canvas</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="columns small-12 medium-2">
+                    <h4>H</h4>
+                    <ul>
+                        <li>
+                            <a href="#">Hunter Douglas Canvas</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="columns small-12 medium-2">
+                    <h4>P</h4>
+                    <ul>
+                        <li>
+                            <a href="#">Plaza Plus</a>
+                        </li>
+                        <li>
+                            <a href="#">Pravada Internal Fabric</a>
+                        </li>
+                        <li>
+                            <a href="#">Primed Artist Canvas</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="columns small-12 medium-2">
+                    <h4>J</h4>
+                    <ul>
+                        <li>
+                            <a href="#">Jute</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="columns small-12 medium-2">
+                    <h4>R</h4>
+                    <ul>
+                        <li>
+                            <a href="#">Ramie Internal</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="columns small-12 medium-2">
+                    <h4>K</h4>
+                    <ul>
+                        <li>
+                            <a href="#">Kendall Internal</a>
+                        </li>
+                        <li>
+                            <a href="#">Kerem Internal</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="columns small-12 medium-2">
+                    <h4>L</h4>
+                    <ul>
+                        <li>
+                            <a href="#">Loomstate Canvas</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="columns small-12 medium-2">
+                    <h4>T</h4>
+                    <ul>
+                        <li>
+                            <a href="#">Tonneau Fabrics</a>
+                        </li>
+                        <li>
+                            <a href="#">Truck Mesh</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        <div>
     </div>
+    
 </section>
---
+++ public/testpage.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Option Marine</title>
+    <link rel="stylesheet" href="http://localhost/library/css/styles.css">
+    <link rel="stylesheet" href="http://localhost/library/css/styles.css?ver=<?php echo rand();?>">
+</head>
+
+<body class="gray-background">
+    <header class="white-background header-secondary">
+        <div class="row align-middle">
+            <div class="columns small-12 medium-4">
+                <div class="header-mast__logo-holder">
+                    <a href="/index.php">
+                        <img class="header-secondary__logo header-mast__logo-holder__logo" src="../images/logo.png" alt="logo" />
+                    </a>
+                </div>
+            </div>
+           <div class="columns small-12 medium-12 test-menu">
+                <nav>
+                    <ul class="main-menu">
+                        <li class="main-menu-child"><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li class="main-menu-child"><a href="#">Projects</a>
+                            <ul class="submenu">
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">About</a></li>
+                                <li class="main-menu-child"><a href="#">Projects</a>
+                                     <ul class="submenu">
+                                        <li><a href="#">Home</a></li>
+                                        <li><a href="#">About</a></li>
+                                        <li><a href="#">Projects</a></li>
+                                    <ul>
+                                </li>
+                            <ul>
+                        </li>
+                    </ul>
+                </nav>
+           </div>
+        </div>
+    </body>
+</html>
